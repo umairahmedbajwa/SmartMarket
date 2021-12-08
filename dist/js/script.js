cf6a5b8257bd1c8c5ba93a6d17db5bb64e263364
umairahmedbajwa <umairahmed.bajwa@systemsltd.com>
2021-12-08 20:38:12 +0500

    Carousel added

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,6 +15,63 @@
 // 	},
 // });
 
-// $(document).ready(function () {
-// 	$(".owl-carousel").owlCarousel();
-// });
+$(document).ready(function () {
+	$(".owl-carousel").owlCarousel();
+});
+
+$(".owl-carousel-5").owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 3,
+		},
+		800: {
+			items: 4,
+		},
+		1000: {
+			items: 5,
+		},
+	},
+});
+
+$(".owl-carousel-6").owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 3,
+		},
+		800: {
+			items: 5,
+		},
+		1000: {
+			items: 6,
+		},
+	},
+});
+
+$(".owl-carousel-1").owlCarousel({
+	loop: true,
+	margin: 10,
+	nav: true,
+	responsive: {
+		0: {
+			items: 1,
+		},
+		600: {
+			items: 1,
+		},
+		1000: {
+			items: 1,
+		},
+	},
+});
